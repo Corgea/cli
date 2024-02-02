092d5a0b72258dbead26cc43d560f9c4c8d89953
Adam Bronte <adam@corgea.com>
2024-02-02 15:24:08 -0800

    - version 1.0 - update build scripts to create zip files for binaries

--- build_darwin.sh
+++ build_darwin.sh
@@ -2,3 +2,6 @@
 
 cargo build -r --target aarch64-apple-darwin
 cargo build -r --target x86_64-apple-darwin
+
+zip corgea-aarch64-apple-darwin.zip target/aarch64-apple-darwin/release/corgea
+zip corgea-x86_64-apple-darwin.zip target/x86_64-apple-darwin/release/corgea
--- build_linux_win.sh
+++ build_linux_win.sh
@@ -3,3 +3,7 @@
 cross build -r --target x86_64-pc-windows-gnu
 cross build -r --target x86_64-unknown-linux-gnu
 cross build -r --target aarch64-unknown-linux-gnu
+
+zip corgea-aarch64-unkown-linux-gnu.zip target/aarch64-unknown-linux-gnu/release/corgea
+zip corgea-x86_64-unknown-linux-gnu.zip target/x86_64-unknown-linux-gnu/release/corgea
+zip corgea-x86_64-pc-windows-gnu.zip target/x86_64-pc-windows-gnu/release/corgea.exe
