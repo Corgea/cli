964a9cbf69e0aef9e9cf9773c1af2fa426f30be2
Leen Kilani <leen.kilani@gmail.com>
2026-09-16 10:04:23 +0300

    COR-2000: update default exlude globs

--- src/utils/generic.rs
+++ src/utils/generic.rs
@@ -42,7 +42,6 @@ const DEFAULT_EXCLUDE_GLOBS: &[&str] = &[
     "**/specs/**",
     "**/node_modules/**",
     "**/tmp/**",
-    "**/migrations/**",
     "**/python*/site-packages/**",
     "**/*.mmdb",
     "**/*.css",
